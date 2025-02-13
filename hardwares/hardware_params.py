c6f3c38b642e6c005696432d00fcda8df793c891
flyer103 <flyer103@gmail.com>
2025-02-13 18:26:47 +0800

    add l20/h20 hardware and correct onchip_buffer data
    Signed-off-by: flyer103 <flyer103@gmail.com>

--- hardwares/hardware_params.py
+++ hardwares/hardware_params.py
@@ -4,11 +4,11 @@ hardware_params = {
     # NOTICES: For GPU, we use Register File Size as on-chip buffer size
     # https://images.nvidia.com/content/volta-architecture/pdf/volta-architecture-whitepaper.pdf
     # NOTICE: V100 not support INT8 in tensor core, so INT8 performance is not good
-    "nvidia_V100": {"bandwidth": 900e9, "FP16": 112e12, "INT8": 62e12, "onchip_buffer": 20480e3},
+    "nvidia_V100": {"bandwidth": 900e9, "FP16": 112e12, "INT8": 62e12, "onchip_buffer": 21504e3},
     # https://images.nvidia.com/aem-dam/en-zz/Solutions/technologies/NVIDIA-ADA-GPU-PROVIZ-Architecture-Whitepaper_1.1.pdf
     "nvidia_A6000": {"bandwidth": 768e9, "FP16": 154.8e12, "INT8": 309.7e12, "onchip_buffer": 21504e3},
     # https://images.nvidia.com/aem-dam/en-zz/Solutions/technologies/NVIDIA-ADA-GPU-PROVIZ-Architecture-Whitepaper_1.1.pdf
-    "nvidia_A6000_Ada": {"bandwidth": 960e9, "FP16": 364.2e12, "INT8": 728.5e12, "onchip_buffer": 36352e3},
+    "nvidia_A6000_Ada": {"bandwidth": 960e9, "FP16": 364.2e12, "INT8": 728.5e12, "onchip_buffer": 32768e3},
     # https://images.nvidia.com/aem-dam/en-zz/Solutions/data-center/nvidia-ampere-architecture-whitepaper.pdf
     # Ampere's SM has 256KB RF, max 164KB Shared Mem
     "nvidia_A100": {"bandwidth": 1555e9, "FP16": 312e12, "INT8": 624e12, "onchip_buffer": 27648e3},  # use 40G data
@@ -27,7 +27,11 @@ hardware_params = {
     "nvidia_H100_PCIe": {"bandwidth": 2048e9, "FP16": 1513e12 / 2, "INT8": 3026e12 / 2, "onchip_buffer": 29184e3},
     # https://images.nvidia.com/aem-dam/Solutions/Data-Center/l4/nvidia-ada-gpu-architecture-whitepaper-v2.1.pdf
     # Ada SM has 256 KB Register File, and 128 KB of L1/Shared Memory
-    "nvidia_L40": {"bandwidth": 864e9, "FP16": 181e12, "INT8": 362e12, "onchip_buffer": 36352e3},
+    "nvidia_L40": {"bandwidth": 864e9, "FP16": 181e12, "INT8": 362e12, "onchip_buffer": 32768e3},
+    "nvidia_L20": {"bandwidth": 864e9, "FP16": 119.5e12, "INT8": 239e12, "onchip_buffer": 23552e3},
+    # The industry has not clearly stated the size of onchip_buffer, but it is estimated to be 24~29MB. 
+    # The minimum value is taken here.
+    "nvidia_H20": {"bandwidth": 4000e9, "FP16": 148e12, "INT8": 296e12, "onchip_buffer": 24576e3},
     # Intel Skylake-X (Skylake-X, Cascade Lake) Intel Xeon Phi (Knights Landing, Knights Mill) Intel Ice Lake, Tiger Lake and Rocket Lake
     # support AVX-512 & FMA (512-bit), they has throughput of 1 cycle
     # https://www.intel.com/content/www/us/en/products/sku/230496/intel-core-i913900k-processor-36m-cache-up-to-5-80-ghz/specifications.html
